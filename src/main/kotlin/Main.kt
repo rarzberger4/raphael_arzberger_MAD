37594fe73b1fc68e9527cf9146b3c64ac4e755c3
rarzberger4 <arzberger98@gmail.com>
2023-02-28 16:32:56 +0100

    random number generator added

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,31 @@
+import kotlin.random.Random
+
 fun main(args: Array<String>) {
     println("Hello World!")
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
+
+
+    var num = 0
+    do{
+        var rnd = (0..9).shuffled().take(4).joinToString("")
+        num = rnd as Int
+    }while ((rnd as Int) < 1000);
+
+
+
+
+
+
+    var userNum = readln();
+
+    println("$num---$userNum")
+
+
+
+
+
+
 }
