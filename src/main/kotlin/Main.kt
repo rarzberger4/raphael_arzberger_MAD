3eb7cb4ce2c7b83442402c12a0868a9d5d3e8879
rarzberger4 <arzberger98@gmail.com>
2023-02-28 17:16:27 +0100

    added strongCheck for directl matching digits

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,46 @@
 import kotlin.random.Random
+fun checkStrongMatch(rndNum: Int, userNum: Int): Int {
+    var rnd = rndNum
+    var usr = userNum
+    var matches = 0
+    var round = 1
+
+    do{
+        if(firstDigit(usr) == firstDigit(rnd)){
+            matches++
+        }
+
+        rnd %= (10000 / (pow(10, round)))  //cuts the first digit each round
+        usr %= (10000 / (pow(10, round)))
+
+        round++
+    }while(usr >= 1)
+
+
+
+    return matches;
+}
+fun firstDigit(n: Int): Int{
+    var ret = n;
+    while(ret > 10){      //gets the first digit of a number
+        ret /= 10;
+    }
+    return ret;
+}
+fun checkWeakMatch(): Int {
+    return 0;
+}
+
+fun pow(num: Int, exp: Int): Int{
+    var expo = exp
+    var result: Long = 1
+    while (expo != 0) {
+        result *= num.toLong()
+        --expo
+    }
+    return result.toInt()
+}
+
 
 fun main(args: Array<String>) {
     println("Hello World!")
@@ -9,21 +51,20 @@ fun main(args: Array<String>) {
 
 
     var num = 0
-    do{
+    do {
         var rnd = (0..9).shuffled().take(4).joinToString("")
-        num = rnd as Int
-    }while ((rnd as Int) < 1000);
+        num = rnd.toInt()
+    } while (num < 1000)
+
+    num = 1234
+    println("Guess a number: ")
+    var userNum = readln().toInt()
 
 
 
+    var result = checkStrongMatch(num as Int, userNum as Int)
 
-
-
-    var userNum = readln();
-
-    println("$num---$userNum")
-
-
+    println("$num---$userNum --> $result")
 
 
 
